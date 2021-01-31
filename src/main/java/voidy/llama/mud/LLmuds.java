8dabdc8bb0f9f6249f126f60636d1b2e023ef619
VoidyCD <pvadiscordteam@gmail.com>
2021-01-31 18:08:46 +0300

    README

--- src/main/java/voidy/llama/mud/LLmuds.java
+++ src/main/java/voidy/llama/mud/LLmuds.java
@@ -8,6 +8,9 @@ import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ShovelItem;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ToolItem;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -28,8 +31,21 @@ public class LLmuds implements ModInitializer {
 
 	public static final Item crack = new Item(new Item.Settings().group(ItemGroup.FOOD).food(mudfood.crack));
 
+	public static final Block crack_block = new Block(FabricBlockSettings.of(Material.WOOL).strength(0.7F, 0.8F)
+			.sounds(BlockSoundGroup.WOOL).breakByTool(FabricToolTags.SHOVELS));
+
+	public static ToolItem POTATO_SHOVEL = new ShovelItem(LLTools.INSTANCE, 1.5F, 3.0F,
+			new Item.Settings().group(ItemGroup.TOOLS));
+
+	public static ToolItem POTATO_SWORD = new SwordItem(LLSword.INSTANCE, 1, -2.4F,
+			new Item.Settings().group(ItemGroup.COMBAT));
+
 	@Override
 	public void onInitialize() {
+
+		Registry.register(Registry.ITEM, new Identifier("llamas", "shovel_item"), POTATO_SHOVEL);
+
+		Registry.register(Registry.ITEM, new Identifier("llamas", "sword_item"), POTATO_SWORD);
 
 		Registry.register(Registry.ITEM, new Identifier("llamas", "mud_ball"), Mud_ball);
 
@@ -50,6 +66,12 @@ public class LLmuds implements ModInitializer {
 		Registry.register(Registry.ITEM, new Identifier("llamas", "super_mud_block"),
 
 				new BlockItem(super_mud_block, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+
+		Registry.register(Registry.BLOCK, new Identifier("llamas", "crack_block"), crack_block);
+
+		Registry.register(Registry.ITEM, new Identifier("llamas", "crack_block"),
+
+				new BlockItem(crack_block, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
 
 	}
 }
